448987194bd465c4f56fa6c6aaec66409b20f397
Abeer2222 <81580052+Abeer2222@users.noreply.github.com>
2021-09-20 14:23:22 +0300

    Update calculator full.kt

--- calculator full.kt
+++ calculator full.kt
@@ -1,55 +1,46 @@
 import kotlin.random.Random
 
 fun main() {
-    fun IntNumber(num: String): Int {
+    fun numCheck(num: String): Boolean {
         try {
-            return num.toInt()
+            num.toFloat()
+            return true
         } catch (e: Exception) {
-            return 0
+            println("Please enter numbers only")
+        }
+        return false
+    }
+
+    fun calculate(num1: Float, operator: Char, num2: Float): Float {
+        return when (operator) {
+            '+' -> num1 + num2
+            '-' -> num1 - num2
+            '*' -> num1 * num2
+            else -> num1 / num2
         }
     }
 
-    fun sum(n1: Int, n2: Int) {
-        println("$n1 + $n2 = " + (n1 + n2))
-    }
-
-    fun subtraction(n1: Int, n2: Int) {
-        println("$n1 - $n2 = " + (n1 - n2))
-    }
-
-    fun division(n1: Int, n2: Int) {
-        println("$n1 / $n2 = " + ((n1 / n2).toFloat()))
-    }
-
-    fun multiplication(n1: Int, n2: Int) {
-        println("$n1 * $n2 = " + (n1 * n2))
-    }
     while (true) {
-        print("Enter your first number: ")
-        val num1 = IntNumber(readLine()!!)
-        print("Enter the Operator: ")
-        val operator = readLine()
-        println("$num1 $operator")
-        print("Enter your second number: ")
-        val num2 = IntNumber(readLine()!!)
-        when (operator) {
-            "+" -> {
-                sum(num1, num2)
-                break
-            }
-            "*" -> {
-                multiplication(num1, num2)
-                break
-            }
-            "/" -> {
-                division(num1, num2)
-                break
-            }
-            "-" -> {
-                subtraction(num1, num2)
-                break
-            }
+        print("Enter first number: ")
+        val number1 = readLine()//numCheck(readLine()!!)
+        if (numCheck(number1!!)) {
+            println(number1)
+            print("Enter operator: ")
+            val op = readLine()
+            if (op == "+" || op == "-" || op == "*" || op == "/") {
+                println(number1 + op)
+                print("Enter second number: ")
+                val number2 = readLine()
+                if (numCheck(number2!!)) {
+                    if (number2.toFloat() == 0f && op == "/") {
+                        println("You cannot divide by 0")
+                    } else {
+                        println("$number1 $op $number2 = ${calculate(number1.toFloat(), op[0], number2.toFloat())}")
+                        break
+                    }
+                }
+            } else
+                println("Invalid Operator")
         }
 
-    }
-}
+    }}
