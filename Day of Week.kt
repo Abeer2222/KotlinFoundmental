83ae152987da2ef77fbd0832ba542299777ab973
Abeer2222 <81580052+Abeer2222@users.noreply.github.com>
2021-09-21 11:05:16 +0300

    Create Day of Week.kt

---
+++ Day of Week.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val Week = mapOf(1 to "Sunday", 2 to "Monday", 3 to "Tuesday",4 to "Wednesday", 5 to "Thursday", 6 to "Friday" , 7 to "Sunday")
+   for(day in Week){
+       println(day)
+   }
+
+}
+
+
+
