b1322d9a375907510983c7cd2454670802097013
Abeer2222 <81580052+Abeer2222@users.noreply.github.com>
2021-09-21 11:07:50 +0300

    Update Day of Week.kt

--- Day of Week.kt
+++ Day of Week.kt
@@ -1,7 +1,7 @@
 fun main() {
     val Week = mapOf(1 to "Sunday", 2 to "Monday", 3 to "Tuesday",4 to "Wednesday", 5 to "Thursday", 6 to "Friday" , 7 to "Sunday")
    for(day in Week){
-       println(day)
+       println("${day.key}--> ${day.value}")
    }
 
 }
