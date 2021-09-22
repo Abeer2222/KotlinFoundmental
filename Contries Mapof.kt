2e4152223074c87b6fc2a2bc8132abdf7533ec57
Abeer2222 <81580052+Abeer2222@users.noreply.github.com>
2021-09-22 09:30:34 +0300

    Update Contries Mapof.kt

--- Contries Mapof.kt
+++ Contries Mapof.kt
@@ -1,50 +1,21 @@
-import kotlin.random.Random
-
 fun main() {
-    class Country(val name: String,val capital: String) {
-        fun print() {
-            println("$name's capital is $capital")
-        }
+    val countries = mutableMapOf<String, String>()
+    for (i in 0..2) {
+        var country = ""
+        var capital = ""
+        print("Enter a country:  ")
+        country = readLine()!!
+        print("Enter the capital:  ")
+        capital = readLine()!!
+
+        countries[country] = capital
     }
 
-    val countries = listOf(
-        Country("Saudi Arabia", "Riyadh"), Country("Egypt", "Cairo"),
-        Country("Spain", "Madrid"), Country("France", "Paris"),
-        Country("Kuwait", "Kuwait"), Country("UA", "Abu dhabi"),
-        Country("Qatar", "Doha"),
-        Country("UK", "London"),
-        Country("USA", "D.C."), Country("Cuba", "Havana")
-    )
-
-    //for(c in countries.indices)
-    //var j=0
-    var q = 0
-    var score = 0
-    while (true) {
-        val randomCourtny = countries[Random.nextInt(countries.size)]
-        println("What is the capital of ${randomCourtny.name}?")
-        q++
-        val answer = readLine()!!
-        if (answer == randomCourtny.capital) {
-            println("Right Answer!!")
-            score++
-        } else {
-            println("Wrong Answer :(")
-            println("Actually!!")
-            randomCourtny.print()
-            println("-------------------------------")
-        }
-        if (q >= 3) {
-            println("You're score: $score ")
-            q = 0
-            score = 0
-            print("do you want another chance? (Y/N)")
-            val chance= readLine()!!
-            if (chance.uppercase() != "Y") {
-                println("Game Over")
-                break
-            }
-        }
+    for (c in countries) {
+        println("${c.key}'s capital is ${c.value}.")
     }
+
 }
+
+
 
